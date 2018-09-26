324f7361472ea576ad3589379d590b6b4e8ef7e5
Jeevan Shriram <jshriram@codeaurora.org>
2018-09-26 15:01:49 -0700

    QcomModulePkg: dm-verity's EIO mode is not applicable for AVB2.0
    With AVB 2.0 feature enabled, all dm-verity errors are treated as
    critical and is never allowed to boot. This change limits EIO errors to
    legacy verified boot version where device is allowed to boot in logging
    mode.
    
    Change-Id: Ia9de4a10caf9ac451de865c5f2def0b75c4c135a

--- QcomModulePkg/Library/avb/VerifiedBoot.c
+++ QcomModulePkg/Library/avb/VerifiedBoot.c
@@ -1143,7 +1143,8 @@ DisplayVerifiedBootScreen (BootInfo *Info)
   }
 
   /* dm-verity warning */
-  if (!IsEnforcing () &&
+  if ((GetAVBVersion () != AVB_2) &&
+      !IsEnforcing () &&
      !Info->BootIntoRecovery) {
     Status = DisplayVerifiedBootMenu (DISPLAY_MENU_EIO);
       if (Status == EFI_SUCCESS) {
